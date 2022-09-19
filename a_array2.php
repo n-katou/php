ec785f1e9237c0b6b947d7a71b89cff02965fcdc
n-katou <102382177+n-katou@users.noreply.github.com>
2022-09-20 01:10:21 +0900

    関数まで完了

---
+++ a_array2.php
@@ -0,0 +1,6 @@
+<?php
+$student = ["name" => "Andy", "age" => 20];
+$student["height"] = 180;
+
+// echo $student["height"] . PHP_EOL;
+echo count($student);
---
+++ a_array3.php
@@ -0,0 +1,5 @@
+<?php
+$student = ["name" => "Andy", "age" => 20];
+$student["age"] = 21;
+
+echo $student["age"];
