f50f1e6096852a732db6c2bbdeff55320c40ad3d
n-katou <102382177+n-katou@users.noreply.github.com>
2022-09-30 00:01:44 +0900

    TO DO リストまで完了

---
+++ function_tr2.php
@@ -0,0 +1,18 @@
+<?php
+
+// function repeat($str, $count) {
+//   $result = "";
+//   for ($i = 0; $i < $count; $i++) {
+//     $result = $result . $str;
+//   }
+//   return $result;
+// }
+
+
+
+$hello2 = str_repeat("Hello", 2);
+echo $hello2 . PHP_EOL;
+
+$world = str_repeat("world", 3);
+echo $world . PHP_EOL;
+
