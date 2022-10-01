3abb1891b7311242669200c938640adf0af8a26f
n-katou <102382177+n-katou@users.noreply.github.com>
2022-10-02 03:34:54 +0900

    webアプリケーションまで完了

---
+++ kousyuu4/web2.php
@@ -0,0 +1,25 @@
+<?php
+$names = file("names.txt", FILE_IGNORE_NEW_LINES);
+$date = date("Y-m-d H:i:s");
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP Sample</title>
+</head>
+<body>
+  <h1>Names</h1>
+  <ul>
+    <?php
+      for ($i = 0; $i < count($names); $i++) {
+    ?>
+    <li><?php echo $names[$i]; ?></li>
+    <?php
+      }
+    ?>
+  </ul>
+  <hr>
+  <p><?php echo $date; ?></p>
+</body>
+</html>
---
+++ kousyuu4/web_tr1.php
@@ -0,0 +1,13 @@
+<?php
+$date = date("Y-m-d H:i:s");
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP Sample</title>
+</head>
+<body>
+  <h1> Date: <?php echo $date; ?></h1>
+</body>
+</html>
